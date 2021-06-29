6053402bf6a0c521e0f376ca973c7b15d951d162
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-06-29 10:33:22 +0100

    task-management without database

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,69 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus} from './tasks.model';
+import { v4 as uuid} from 'uuid'
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[]{
+        return this.tasks;
+    }
+
+    getTaskById(id:  string): Task{
+        const found = this.tasks.find(( task => task.id === id))
+
+        if(!found){
+            throw new NotFoundException(`Task with ID : ${id} not found`);
+        }
+
+        return found;
+    }
+
+    getTasksWithFilter(filterDto: GetTaskFilterDto): Task[]{
+        const {status, search} = filterDto;
+        let tasks = this.getAllTasks();
+        if(status){
+            tasks = tasks.filter((task)=> task.status === status)
+        }
+
+        if(search){
+            tasks = tasks.filter((task)=>{
+                if(task.title.includes(search) || task.description.includes(search)){
+                    return true;
+                }
+                return false;
+            })
+        }
+
+        return tasks;
+    }
+
+    createTask(creatTaskDto:  CreateTaskDto): Task{
+        const {title, description} = creatTaskDto
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+        this.tasks.push(task)
+        return task;
+    }
+
+    deleteTask(id:  string): void{
+        const found = this.getTaskById(id)
+        this.tasks = this.tasks.filter(( task => task.id !== found.id))
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus){
+        const task = this.getTaskById(id)
+        task.status =  status
+        if(!task.status){
+
+        }
+        return task;
+    }
+}
